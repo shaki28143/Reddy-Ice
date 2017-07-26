c994e8205c84c4931ced7e72a48ab50da808f3d0
Shakir Hussain <shakir.h@HCL.COM>
2017-07-26 19:32:37 +0530

    code with dashbord

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'dashboard-cmp',
     moduleId: module.id,
-    templateUrl: 'dashboard.component.html'
+    templateUrl: 'dashboard.component.html',
+    styleUrls:['dashboard.component.scss']
 })
 
 export class DashboardComponent{}
