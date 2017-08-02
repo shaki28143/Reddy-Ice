7eaca2d677ce759a7187f3f404dc474a59f02246
Rishabh Rastogi <rishabh_r@HCL.COM>
2017-08-02 16:17:36 +0530

    sorting icon

--- src/app/dashboard/customer/customer-management/customer-management.component.ts
+++ src/app/dashboard/customer/customer-management/customer-management.component.ts
@@ -2,6 +2,7 @@ import { CustomerService } from '../customer.service';
 import { Component, NgModule, OnInit } from '@angular/core';
 
 @Component({
+    moduleId: module.id,
     selector:'customer',
     templateUrl:'./customer-management.component.html',
     styleUrls:['./customer-management.scss']
@@ -9,7 +10,10 @@ import { Component, NgModule, OnInit } from '@angular/core';
 export class CustomerManagementComponent implements OnInit{
     public customers:any;
     public filterText: string = "";
-    public isAsc: boolean = false;
+    public isAsc: boolean = true;
+    public customer = {
+        sortField : 'customerId'
+    };
     constructor(private customerService:CustomerService){
         customerService.getCustomers().subscribe(
             val => this.customers = val,
